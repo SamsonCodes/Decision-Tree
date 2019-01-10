fa898977ca9cd80c41cc8b4b190cd1a5e20ecbdf
Samson Daniel <44621365+SamsonCodes@users.noreply.github.com>
2019-01-10 09:36:40 +0100

    Simple Dataset Finetuning In Progress

--- decisiontree.py
+++ decisiontree.py
@@ -6,10 +6,23 @@ Created on Wed Jan  2 10:24:24 2019
 """
 import random
 import pandas as pd
+from operator import itemgetter
+from tkinter import *  
 
+FRAME_WIDTH = 1200
+FRAME_HEIGHT = 800
+"""
 dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
 data = pd.read_csv(dataset_url, sep=';')
 raw_data = data.values
+targetVariable = 11
+"""
+
+#Test data override
+raw_data = [[0,0,0],[0,1,0],[1,0,0],[1,1,1]]
+data = pd.DataFrame(data = raw_data, columns = ["A","B","C"], copy = False)
+targetVariable = 2
+targetCategories = [[0,0.5],[0.5,1]]
 
 def getRanges(dataSet):
     range_min = []
@@ -59,16 +72,17 @@ class Tree:
             #print("self.depth=",self.depth, "len(self.nodes)=",len(self.nodes))
             for x in range(0, len(self.nodes)):
                 #print("x=",x,",len(self.nodes[x].dataSet)=",len(self.nodes[x].dataSet))
-                if(self.nodes[x].layer == self.depth and len(self.nodes[x].dataSet) >= 2):    
-                    self.nodes[x].split()
-                    subsets = getSubsets(self.nodes[x].dataSet, self.nodes[x].feature, self.nodes[x].value)
-                    if not (hasEmptySets(subsets)):
-                        child1 = Node(index, self.depth+1, subsets[0])
-                        index+=1
-                        self.nodes.append(child1)
-                        child2 = Node(index, self.depth+1, subsets[1])
-                        index+=1
-                        self.nodes.append(child2)
+                if(self.nodes[x].layer == self.depth):    
+                    if(len(self.nodes[x].dataSet) >= 2):
+                        self.nodes[x].split()
+                        subsets = getSubsets(self.nodes[x].dataSet, self.nodes[x].feature, self.nodes[x].value)
+                        if not (hasEmptySets(subsets)):
+                            child1 = Node(index, self.depth+1, subsets[0])
+                            index+=1
+                            self.nodes.append(child1)
+                            child2 = Node(index, self.depth+1, subsets[1])
+                            index+=1
+                            self.nodes.append(child2)
             self.depth+=1        
     def printTree(self):        
         for layer in range(1, self.depth + 1):
@@ -86,38 +100,72 @@ class Tree:
                 printDataStats(node.dataSet)
                 print()
         print("\n") 
+    def draw(self, canvas):
+        for layer in range(1, self.depth + 1): 
+            x = 0
+            for node in self.nodes:                
+                if(node.layer == layer):    
+                    x+=1
+                    recWidth = 100
+                    recHeight = 70
+                    if(node.feature != -1):                        
+                        lines = [node.name, str(data.columns[node.feature]) + "<" + "{:.2f}".format(node.value),"gini =" + "{:.2f}".format(node.gini),"data set size =" + str(len(node.dataSet))]
+                    else:
+                        lines = [node.name, "gini =" + "{:.2f}".format(node.gini),"data set size =" + str(len(node.dataSet))]
+                    node.draw(x*recWidth*1.1 - recWidth/2,layer*recHeight*1.1 - recHeight/2,recWidth, recHeight, lines, canvas)
 
 class Node:
     def __init__(self, index, layer, dataSet):
-        self.name = "Node" + str(index)
+        firstRowCategory = getCategory(dataSet[0][targetVariable]) #used in the for loop to determine whether or not the dataset is pure       
+        self.leaf = True #Leaf until proven otherwise
+        print("node",index) 
+        #print("dataSet=",dataSet)
+        for row in dataSet:
+            print(row[targetVariable])
+            if(getCategory(row[targetVariable]) != firstRowCategory): #if the data set is not pure
+                if(self.leaf):
+                    print("firstRowCategory=",firstRowCategory, "this row=",row)
+                self.leaf = False #then this node is not a leaf
+        
+        if(self.leaf):
+            self.name = "Leaf" + str(index)
+        else:            
+            self.name = "Node" + str(index)
+        print("classified as",self.name)
         self.layer = layer      
         self.dataSet = dataSet
         self.feature = -1
         self.value = 999
         self.gini = 1
+    def draw(self, drawX, drawY, recWidth, recHeight, lines, canvas):
+        recColor = 'white'                
+        canvas.create_rectangle(drawX,drawY,drawX+recWidth,drawY+recHeight, fill = recColor)
+        for i in range(0, len(lines)):            
+            canvas.create_text(drawX + recWidth/2,drawY + 10 + i * (recHeight/len(lines)),fill="black",font="Times 8 italic bold",
+                                text=lines[i])
+        
         
     def split(self):
         ranges = getRanges(self.dataSet)
         range_min = ranges[0]
         range_max = ranges[1]
-        intervals = 100
+        intervals = 10
         giniScores = []
         for f in range(0, len(self.dataSet[0])):
-            for i in range(0,intervals):
-                splitFeature = f
-                splitValue = range_min[f] + i*(range_max[f] - range_min[f])/1000
-                subsets = getSubsets(self.dataSet, splitFeature, splitValue)
-                if hasEmptySets(subsets):
-                    gini = 1 #no empty sets please!
-                else:
-                    gini = random.uniform(0,1) #gini index is random for now!
-                giniScores.append([splitFeature, splitValue, gini])        
+            if (f != targetVariable): # Obviously we don't want to split on our target variable
+                for i in range(0,intervals):
+                    splitFeature = f
+                    splitValue = range_min[f] + i*(range_max[f] - range_min[f])/intervals
+                    subsets = getSubsets(self.dataSet, splitFeature, splitValue)
+                    if hasEmptySets(subsets):
+                        gini = 1 #no empty sets please!
+                    else:
+                        gini = random.uniform(0,1) #gini index is random for now!
+                    giniScores.append([splitFeature, splitValue, gini])
+        
+        giniScores.sort(key = itemgetter(2)) #Sorts the list by gini score in ascending order
         gini_min_id = 0
-        gini_min = giniScores[0][2]
-        for g in range(0, len(giniScores)):
-            if giniScores[g][2] < gini_min:
-                gini_min_id = g
-                gini_min = giniScores[g][2]
+        
         #print("gini_min_id",gini_min_id,"gini_min",giniScores[gini_min_id])
         self.feature = giniScores[gini_min_id][0]
         self.value = giniScores[gini_min_id][1]
@@ -140,15 +188,37 @@ def hasEmptySets(subsets):
             return True
     return False  
 
+def getCategory(targetValue):
+    if isinstance(targetCategories[0], list):  #If categories are intervals           
+        for interval in targetCategories:
+            if targetValue > interval[0] and targetValue < interval[1]:
+                return targetCategories.index(interval) #return what interval it is in
+    else: #If categories are simply values
+        return targetValue #Simply return the value
+
 #MAIN PROGRAM
 print("running!")
 
-for i in range(0, 10): 
+for i in range(0, 1): 
     print("Tree",i+1)      
     tree = Tree()
     tree.printTree()
     print("\n")
     #tree.printSubsets()   
+
+getCategory(0)
+    
+window = Tk()
+window.title("Tree of Wisdom")
+
+#gui.geometry(str(FRAME_WIDTH) + "x" + str(FRAME_HEIGHT))
+#f1.printField()T))
+
+canvas = Canvas(window ,width=FRAME_WIDTH ,height=FRAME_HEIGHT)
+canvas.pack()
+tree.draw(canvas)
+
+window.mainloop()
 
 print("done!")
 
