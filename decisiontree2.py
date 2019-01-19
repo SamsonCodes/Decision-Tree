90588fbb4d6bf38b73e4e58182d5bcc371836d66
Samson Daniel <44621365+SamsonCodes@users.noreply.github.com>
2019-01-19 09:40:50 +0100

    Drawing recursively is tricky

--- decisiontree2.py
+++ decisiontree2.py
@@ -14,38 +14,61 @@ FRAME_WIDTH = 1200
 FRAME_HEIGHT = 800
 operators = ['==','<','>']
 
-ABC = [[0,0,0],[0,1,0],[1,0,0],[1,1,1]]
-data = pd.DataFrame(data = ABC, columns = ["A","B","C"], copy = False)
-targetCategories = [0, 1]
-stepAmount = 10
-targetIndex = 2
+Golf = [[0,2,1,0,0],
+        [0,2,1,1,0],
+        [1,2,1,0,1],
+        [2,1,1,0,1],
+        [2,0,0,0,1],
+        [2,0,0,1,0],
+        [1,0,0,1,1],
+        [0,1,1,0,0],
+        [0,0,0,0,1],
+        [2,1,0,0,0],
+        [0,1,0,1,1],
+        [1,1,1,1,1],
+        [1,2,0,0,1],
+        [2,1,1,1,0]]
+data = pd.DataFrame(data = Golf, columns = ["Outlook","Temp","Humidity","Windy","Play"], copy = False)
+targetIndex = 4
+targetCategories = [0,1]
 
 class Leaf:
     def __init__(self, rows):
         print("I am a cute little Leaf!")
         self.rows = rows
-    def printLeaf(self):
+    def printMe(self):
         print("Leaf :", self.rows)
-
+    def drawMe(self, drawX, drawY, size, layer, canvas):            
+        recColor = 'green'  
+        canvas.create_oval(drawX - size/2, drawY + size/2, 
+                                drawX + size/2, drawY - size/2,
+                                fill = recColor)
 class Node:
     def __init__(self, question, trueBranch, falseBranch):
         print("I am a cute little Node!")
         self.question = question
         self.trueBranch = trueBranch
         self.falseBranch = falseBranch
-    def printNode(self):
+    def printMe(self):
         print("Node: " + self.question.text())
-        if(isinstance(self.trueBranch, Node)):
-            self.trueBranch.printNode()
-        else:
-            self.trueBranch.printLeaf()
-        if(isinstance(self.falseBranch, Node)):
-            self.falseBranch.printNode()
-        else:
-            self.falseBranch.printLeaf()
-        
-        
-        
+        self.trueBranch.printMe()
+        self.falseBranch.printMe()
+    def drawMe(self, drawX, drawY, size, layer, canvas):            
+        recColor = 'brown'  
+        trueX = drawX - size/1.8 - layer*size/1.8
+        trueY = drawY + size*1.1
+        falseX = drawX + size/1.8 + layer*size/1.8
+        falseY = trueY
+        canvas.create_line(drawX, drawY, trueX, trueY,
+                                fill = 'black')
+        canvas.create_line(drawX, drawY, falseX, falseY,
+                                fill = 'black')
+        canvas.create_rectangle(drawX - size/2, drawY + size/2, 
+                                drawX + size/2, drawY - size/2,
+                                fill = recColor)
+        self.trueBranch.drawMe(trueX, trueY, size, layer + 1, canvas)
+        self.falseBranch.drawMe(falseX, falseY, size, layer + 1, canvas)
+            
 class Question:
     def __init__(self, operator, valueIndex, splitValue):
         self.operator = operator
@@ -67,6 +90,7 @@ def buildTree(rows):
 def findBestSplit(targetCategories, targetVariable, rows): 
     bestGain = None
     bestQuestion = None
+    stepAmount = 10
     for operator in operators:
         for variableIndex in range(0, len(rows[0])):            
             if not (variableIndex == targetIndex):
@@ -174,15 +198,17 @@ print(data.head())
 print("\n")
 
 tree = buildTree(data.values)
-tree.printNode()
-"""
+tree.printMe()
+
 window = Tk()
 window.title("Tree of Wisdom")
 
 canvas = Canvas(window ,width=FRAME_WIDTH ,height=FRAME_HEIGHT)
 canvas.pack()
+size = FRAME_WIDTH/20
+tree.drawMe(FRAME_WIDTH/2, size/2.2, size, 0, canvas)
 
 window.mainloop()
-"""
+
 
 print("done!")
